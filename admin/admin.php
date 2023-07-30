fb2291d324c9d4703d52473026fa8c4f3e50ccc0
trucbntdev <trucbntph33131@gmail.com>
2023-07-30 22:55:53 +0700

    ad upload modify admin

--- admin/admin.php
+++ admin/admin.php
@@ -1,3 +1,22 @@
+<?php
+$err = $maProduct =   "";
+if (isset($_GET['error'])) {
+    $err = $_GET['error'];
+    $maProduct = $_GET['maProduct'];
+}
+try {
+    $conn = new PDO("mysql:host=localhost;dbname=mydatabase", "root", "");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $query = "SELECT * FROM product";
+    $stmt =  $conn->prepare($query);
+    $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    $stmt->execute();
+    $products =  $stmt->fetchAll();
+} catch (PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -8,8 +27,42 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     <style>
-        img{
-            width: 100px;
+        img {
+            width: 70px;
+            height: 50px;
+            object-fit: fill;
+        }
+
+        .formUpload {
+            width: 500px;
+            top: 100px;
+            left: calc(50% - 250px);
+            padding: 15px;
+            border: 1px solid #ccc;
+            box-shadow: 0 0 10px #2e2e2e;
+            border-radius: 5px;
+            position: absolute;
+            z-index: 100;
+            background-color: #fff;
+        }
+
+        .btn-primary {
+            margin: 0 auto;
+        }
+
+        .btnAddproduct {
+            margin: 20px auto;
+            display: block;
+            height: 40px;
+            border: 1px solid #ccc;
+        }
+
+        .err-message {
+            color: red;
+        }
+
+        .descript {
+            width: 400px;
         }
     </style>
 </head>
@@ -18,37 +71,72 @@
     <table class="table">
         <thead>
             <tr>
-                <th>STT</th>
-                <th>IMG product</th>
+                <th>code Product</th>
+                <th>Img Product</th>
                 <th>Name product</th>
-                <th>Price</th>
+                <th>Price Old</th>
+                <th>Price New</th>
+                <th>Description</th>
                 <th>Action</th>
             </tr>
         </thead>
         <tbody>
-            <tr>
-                <td>1</td>
-                <td><img src="../fontEnd/img/sp1.png" alt=""></td>
-                <td>Bánh mì pate giò đặc biệt</td>
-                <td>25.000đ</td>
-                <td><button class="btn btn-outline-primary">thêm</button></td>
-            </tr>
-            <tr>
-                <td>2</td>
-                <td><img src="../fontEnd/img/sp2.png" alt=""></td>
-                <td>Bánh mì bò hũ tiếu</td>
-                <td>25.000đ</td>
-                <td><button class="btn btn-outline-primary">thêm</button></td>
-            </tr>
-            <tr>
-                <td>2</td>
-                <td><img src="../fontEnd/img/sp3.png" alt=""></td>
-                <td>Bánh mì xá xíu đặc biệt</td>
-                <td>25.000đ</td>
-                <td><button class="btn btn-outline-primary">thêm</button></td>
-            </tr>
+            <?php foreach ($products as $product) : ?>
+                <tr>
+                    <td><?php echo $product['maProduct'] ?></td>
+                    <td><img src="../img/<?php echo $product['imgProduct'] ?>" alt=""></td>
+                    <td><?php echo $product['nameProduct'] ?></td>
+                    <td><?php echo $product['priceNew'] ?></td>
+                    <td><?php echo $product['priceOld'] ?></td>
+                    <td class="descript"><?php echo $product['descript'] ?></td>
+                    <td><button class="btn btn-outline-primary">thêm</button></td>
+                </tr>
+            <?php endforeach; ?>
         </tbody>
     </table>
+    <button class="btnAddproduct btn  btn-primary">Thêm sản phẩm</button>
+    <form action="../model/upload.php" method="POST" class="formUpload toast " enctype="multipart/form-data" id="formadd">
+        <div class="toast-header mb-3">
+            <strong class="me-auto">thêm sản phẩm </strong>
+            <button style="float: right;" type="button" class="btn-close" data-bs-dismiss="toast"></button>
+        </div>
+        <div class=" mb-3">
+            <label for="maProduct" class="form-label">Mã sản phẩm (int) :</label>
+            <input type="number" class="form-control" id="maProduct" placeholder="nhập mã sản phẩm" name="maProduct" required="" value="<?php echo $maProduct ?>">
+            <span class="err-message"><?php echo $err ?></span>
+        </div>
+        <div class="mb-3">
+            <label for="nameProduct" class="form-label">Tên Sản Phẩm :</label>
+            <input type="text" class="form-control" id="nameProduct" placeholder="nhập tên sản phẩm" name="nameProduct" required="">
+        </div>
+
+        <div class="row mb-3">
+            <div class=" col">
+                <label for="priceNew" class="form-label">giá mới :</label>
+                <input type="number" class="form-control" id="priceNew" placeholder="giá mới" name="priceNew" required="">
+            </div>
+            <div class=" col">
+                <label for="priceOld" class="form-label">giá cũ : </label>
+                <input type="number" class="form-control" id="priceOld" placeholder="giá cũ" name="priceOld" required="">
+            </div>
+        </div>
+        <div class="mb-3">
+            <label for="descript">Mô tả sản phẩm : </label>
+            <textarea class="form-control" rows="3" id="descript" name="descript" required=""></textarea>
+        </div>
+        <div class="mb-3">
+            <label for="fieldImg" class="form-label">ảnh sản phẩm : </label> <br>
+            <input type="file" name="fieldImg" id="fieldImg" required="">
+        </div>
+        <button type="submit" class="btn btn-primary" name="btnSubmit">Submit</button>
+    </form>
 </body>
+<script>
+    var btnadd = document.querySelector(".btnAddproduct");
+    var form = document.getElementById("formadd");
+    btnadd.addEventListener("click", function(e) {
+        form.classList.add("show");
+    })
+</script>
 
 </html>
---
+++ model/upload.php
@@ -0,0 +1,87 @@
+<?php
+$target_dir = "../img/";
+$target_file = $target_dir . basename($_FILES["fieldImg"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+
+// Check if image file is a actual image or fake image
+if (isset($_POST["btnSubmit"])) {
+    $check = getimagesize($_FILES["fieldImg"]["tmp_name"]);
+    if ($check !== false) {
+        $uploadOk = 1;
+    } else {
+        echo "file của bạn không phải 1 ảnh";
+        $uploadOk = 0;
+    }
+}
+
+// Check if file already exists
+if (file_exists($target_file)) {
+    echo "ảnh đã tồn tại";
+    $uploadOk = 0;
+}
+
+// Check file size
+// 500000 byte = 500 kb
+if ($_FILES["fieldImg"]["size"] > 500000) {
+    echo "thất bại , kích thước ảnh quá lớn";
+    $uploadOk = 0;
+}
+
+// Allow certain file formats
+if (
+    $imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+    && $imageFileType != "gif"
+) {
+    echo "chỉ chấp nhận ảnh jpg  , png , jpeg , gif";
+    $uploadOk = 0;
+}
+
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+    echo "xin lỗi không thế upload";
+    // if everything is ok, try to upload file
+} else {
+    if (move_uploaded_file($_FILES["fieldImg"]["tmp_name"], $target_file)) {
+        $maProduct = $imgProduct =  $nameProduct = $priceNew = $priceOld  = $descript = "";
+        if (isset($_POST["maProduct"]) && isset($_POST["nameProduct"]) && isset($_POST["priceOld"]) && isset($_POST["priceNew"]) && isset($_POST["descript"])) {
+            $maProduct = $_POST["maProduct"];
+            $nameProduct = $_POST["nameProduct"];
+            $priceNew = $_POST["priceNew"];
+            $priceOld = $_POST["priceOld"];
+            $descript = $_POST["descript"];
+            $imgProduct = $_FILES["fieldImg"]["name"];
+            try {
+                $conn = new PDO("mysql:host=localhost;dbname=mydatabase", "root", "");
+                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                $queryGet = "SELECT * FROM product WHERE maProduct = :maProduct";
+                $stmt0 = $conn->prepare($queryGet);
+                $stmt0->bindParam(":maProduct" , $maProduct);
+                $stmt0->setFetchMode(PDO::FETCH_ASSOC);
+                $stmt0->execute();
+                $result = $stmt0->fetch();
+                if($result){
+                    header("Location: ../admin/admin.php?error=mã sản phẩm đã tồn tại&maProduct=".$maProduct);
+                    exit();
+                }else{
+                    $queryadd = "INSERT INTO product(maProduct,nameProduct,imgProduct,priceNew,priceOld,descript)
+                     VALUES(:maProduct , :nameProduct , :imgProduct ,:priceNew , :priceOld , :descript);";
+                    $stmt = $conn->prepare($queryadd);
+                    $stmt->bindParam(":maProduct", $maProduct);
+                    $stmt->bindParam(":nameProduct", $nameProduct);
+                    $stmt->bindParam(":imgProduct", $imgProduct);
+                    $stmt->bindParam(":priceNew", $priceNew);
+                    $stmt->bindParam(":priceOld", $priceOld);
+                    $stmt->bindParam(":descript", $descript);
+                    $stmt->execute();
+                }
+                echo "successfully executed";
+            } catch (PDOException $e) {
+                echo "Error: " . $e->getMessage();
+            }
+        }
+    } else {
+        echo "xin lỗi tải ảnh thất bại";
+    }
+}
+?>
